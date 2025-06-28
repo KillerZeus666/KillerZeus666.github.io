61b666df6c225f461c0f83b8c33907af8fb18722
KillerZeus666 <katheryng2017@gmail.com>
2025-06-28 10:20:28 -0500

    Agregando header

--- proyecto/src/app/header/header.component.ts
+++ proyecto/src/app/header/header.component.ts
@@ -1,3 +1,4 @@
+// header.component.ts
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  menuOpen = false;
+  language = 'es';
 
+  navTexts = {
+    inicio: 'Inicio',
+    sobreMi: 'Sobre mí',
+    proyectos: 'Proyectos',
+    contacto: 'Contacto'
+  };
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  changeLanguage(event: Event) {
+    // Convertimos el target a <select>
+    const selectEl = event.target as HTMLSelectElement;
+    const lang = selectEl.value;
+
+    this.language = lang;
+
+    if (lang === 'en') {
+      this.navTexts = {
+        inicio: 'Home',
+        sobreMi: 'About me',
+        proyectos: 'Projects',
+        contacto: 'Contact'
+      };
+    } else {
+      this.navTexts = {
+        inicio: 'Inicio',
+        sobreMi: 'Sobre mí',
+        proyectos: 'Proyectos',
+        contacto: 'Contacto'
+      };
+    }
+  }
 }
